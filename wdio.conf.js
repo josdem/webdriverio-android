18ecf286a0ce8d0b763ab3dcd0711bec20c6cce1
josdem <joseluis.delacruz@gmail.com>
2021-08-25 09:40:48 -0400

    Adding desired capabilities for Android

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,13 +52,19 @@ exports.config = {
     //
     capabilities: [{
 
+        "deviceName": "Pixel_3_10.0",
+        "platformVersion": "10",
+        "platformName": "Android",
+        "appPackage": "com.jos.dem.launcher",
+        "appActivity": ".MainActivity"
+
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+        //maxInstances: 5,
         //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
+        //browserName: 'chrome',
+        //acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
