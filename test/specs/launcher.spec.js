1782e1b757b3b162af495f860149017fc046a17e
josdem <joseluis.delacruz@gmail.com>
2021-08-27 11:46:00 -0400

    Validating text view response

--- test/specs/launcher.spec.js
+++ test/specs/launcher.spec.js
@@ -1,3 +1,5 @@
+const assert = require('assert')
+const properties = require('../properties/test.properties')
 const MainPage = require('../pageobjects/main.page')
 
 describe('Should validate launcher', () => {
@@ -5,6 +7,6 @@ describe('Should validate launcher', () => {
     it('validate message changing screen', async() => {
         await MainPage.readMessage()
         const response = await MainPage.changeMessage()
-        console.log("response: ", response)
+        assert.deepEqual(response, properties.message)
     })
 })
