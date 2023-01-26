f1ba12b6e9b594f3944dc0ba954f0a3d76c989ee
josdem <joseluis.delacruz@gmail.com>
2023-01-26 17:13:19 -0500

    Using static link build

--- test/configs/wdio.applitools.conf.js
+++ test/configs/wdio.applitools.conf.js
@@ -9,7 +9,7 @@ config.capabilities = [
     deviceName: "Pixel_6_API_33",
     platformVersion: "13",
     platformName: "Android",
-    app: "https://github.com/josdem/android-launcher/releases/download/v1.4/ready.apk",
+    app: "https://github.com/josdem/android-launcher/releases/download/v1.5/app-debug.apk",
   },
 ]
 
